41a933cdbb4ea8874b4067d34c6de82bffa3e41c
marvanessa1 <marvanessa1@live.com>
2022-04-18 23:49:18 -0700

    started questions.js, questions to be used to build specific role objects

---
+++ src/Questions.js
@@ -0,0 +1,35 @@
+// Questions used to build role objects
+
+// Manager Questions
+const questionsManager = [
+    {
+        type: "input",
+        message: "What is the team Manager's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the team Manager's id?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the team Manager's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the team Manager's office number?",
+        name: "number"
+    },
+]
+
+// Engineer Questions
+
+// Intern Questions
+
+// Prompt: "What would you like to do next?"
+
+// Build main question object that has all the role specific questions and prompts 
+
+module.export = Questions;
