a9d3f97e9c71c1be5b06fde85aaf7ebe4687b401
marvanessa1 <marvanessa1@live.com>
2022-04-18 23:00:30 -0700

    initial files, structured

---
+++ index.js
@@ -0,0 +1,26 @@
+const employee = [];
+
+// Actions need
+
+// Ask them for manager info
+    // Prompt the user for the data
+        // THEN create and store an object for the Manager
+        // THEN "Ask them what they would like to do next"
+
+// Ask them for engineer info
+    // Prompt the user for the data
+        // THEN create and store an object for the Engineer
+        // THEN "Ask them what they would like to do next"
+
+// Ask them for intern info
+    // Prompt the user for the data
+        // THEN create and store an object for the Intern
+        // THEN "Ask them what they would like to do next"
+
+// Ask them what they would like to do next
+    // Add Engineer, Add Intern, Be done
+        // IF "Add Engineer" -> "Ask them for engineer info"
+        // IF "Add Inter" -> "Ask them for intern info"
+        // IF "Be done" -> "build an html page"
+        
+// Use all of the collected employee data to build an html page
