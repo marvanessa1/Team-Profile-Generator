5194709ad672f14564ccaf4b5144538fa14f6e92
marvanessa1 <marvanessa1@live.com>
2022-04-18 23:54:13 -0700

    added questions for enginner and intern role objects

--- src/Questions.js
+++ src/Questions.js
@@ -10,7 +10,7 @@ const questionsManager = [
     {
         type: "input",
         message: "What is the team Manager's id?",
-        name: "name"
+        name: "id"
     },
     {
         type: "input",
@@ -25,9 +25,52 @@ const questionsManager = [
 ]
 
 // Engineer Questions
+const questionsEngineer = [
+    {
+        type: "input",
+        message: "What is the Engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the Engineer's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the Engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the Engineer's github profile?",
+        name: "github"
+    },
+]
 
 // Intern Questions
-
+const questionsIntern = [
+    {
+        type: "input",
+        message: "What is the Intern's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the Intern's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the Intern's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What school is the Intern attending?",
+        name: "school"
+    },
+]
 // Prompt: "What would you like to do next?"
 
 // Build main question object that has all the role specific questions and prompts 
